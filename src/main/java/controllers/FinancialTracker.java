c2b6962ffed6b0bbb6fc2923cb9b5ea3dfa0ca5b
josimpson <45605472+josimpson@users.noreply.github.com>
2019-08-02 09:11:48 -0600

    Started the basic concepts of the financial tracker
    Financial Tracker class started.

---
+++ src/main/java/controllers/FinancialTracker.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.Node;
+import javafx.scene.control.Pagination;
+import javafx.scene.control.TableView;
+import models.OrderedItem;
+
+public class FinancialTracker {
+	//Basic idea of what this should be
+	
+	Pagination myPagination = new Pagination();
+	
+	TableView table = new TableView();
+	
+	ObservableList<OrderedItem> master = FXCollections.observableArrayList();
+	
+	int pageSize = 50;
+	
+	InventoryTrackerController tracker = new InventoryTrackerController();
+	
+	
+	public void init(){
+		//Method to get all inventory items and set them to the ObservableList needed
+		master = tracker.getAllItems();
+		
+		
+		myPagination.setPageFactory(this::createPage);
+	}
+
+	@SuppressWarnings("unchecked")
+	public Node createPage(int pageIndex) {
+		int first = pageIndex * pageSize;
+		int last = Math.min(first + pageSize, master.size());
+		table.setItems(FXCollections.observableArrayList(master.subList(first, last)));
+
+		return table;
+	}
+}
