e606da42d798a3b876550a50907bd441061c0898
Manny <mannypein1@hotmail.com>
2019-08-05 09:02:40 -0600

    Merge branch 'master' of C:\Users\Manny\Documents\GitHub\RestaurantInventoryManager with conflicts.

--- src/main/java/controllers/InventoryTrackerController.java
+++ src/main/java/controllers/InventoryTrackerController.java
@@ -6,23 +6,44 @@ import models.OrderedItem;
 
 import java.util.ArrayList;
 import java.util.Date;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.ServerAddress;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
+import org.bson.Document;
+import java.util.Arrays;
+import com.mongodb.Block;
+
+import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
+import com.mongodb.client.result.DeleteResult;
+import static com.mongodb.client.model.Updates.*;
+import com.mongodb.client.result.UpdateResult;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InventoryTrackerController {
+    MongoClient mc = new MongoClient("localHost");
+    MongoDatabase database = mc.getDatabase("Restaurants");
+    MongoCollection<Document> collection = database.getCollection("Inventory");
 
     public void addItem(int itemId) {
 
     }
 
     public void deleteItem(int itemId, int amount) {
-
+        System.out.println(collection.deleteOne(eq("ingredientID", itemId)));
     }
 
-    public void updateItem(int itemId) {
-
+    public void updateItem(int itemId, String updateField) {
+        collection.updateOne(eq("id", itemId), new Document("$set", new Document(updateField, 110)));
     }
 
     public void getItemData(int itemId) {
-
+        collection.find(eq("ingredientID", itemId)).forEach(printBlock);
     }
 
     public ArrayList<Ingredient> soonToExpire() {
@@ -40,5 +61,13 @@ public class InventoryTrackerController {
     public ObservableList<OrderedItem> reviewOrderedItems(){
     return null;
     }
+
+
+    Block<Document> printBlock = new Block<Document>() {
+        @Override
+        public void apply(final Document document) {
+            System.out.println(document);
+        }
+    };
 
 }
